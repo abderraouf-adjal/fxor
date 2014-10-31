9c3e8e311b1ef01a00dd2945fbb88617c41e07ec
abderraouf-adjal <abderraouf.adjal@gmail.com>
2014-10-31 18:15:08 +0100

    Add bad usage handling in scripts/opssl_rand.sh

--- scripts/opssl_rand.sh
+++ scripts/opssl_rand.sh
@@ -4,9 +4,17 @@
 
 # Usage: opssl_rand.sh IN_FILE_NAME OUT_FILE_NAME
 
-# This script take IN_FILE_NAME size and run openssl command to make a random file (OUT_FILE_NAME)
-# OUT_FILE_NAME_SIZE = IN_FILE_NAME_SIZE
+# This script take IN_FILE_NAME size and run openssl command to make
+# a random file (OUT_FILE_NAME), So OUT_FILE_NAME_SIZE = IN_FILE_NAME_SIZE
 
+
+if ! [ "$#" -eq 2 ]; then
+	printf 'Usage: %s IN_FILE_NAME OUT_FILE_NAME\n\n' "$0"
+	printf 'This script take IN_FILE_NAME size and run openssl command to make\n'
+	printf 'a random file (OUT_FILE_NAME), So OUT_FILE_NAME_SIZE = IN_FILE_NAME_SIZE\n'
+	
+	exit 1;
+fi
 
 openssl_rand_cmd="`stat $1 --format=\"openssl rand %s -out $2\"`"
 
